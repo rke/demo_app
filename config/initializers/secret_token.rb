3134d7a8feb1dd19049956c6a4e4514a1c2638ed
Rainer Keller <rke@fitview.com>
2011-04-09 19:42:51 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '945c145759042d197ddfe3fb260aed4b23cc2756407399068d563b4c95b44221672c58b7b8cfcbe1208219932262dfd1062672f3c81678afed7b79d035d5c990'
